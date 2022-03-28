17f6b269bc0eb34c834f98ab01005e77b93d6b0d
Егор Дедюков <glamber3@mail.ru>
2022-03-28 22:47:57 +0300

    change

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,21 +4,24 @@ const burgerBtn = document.querySelector('.hamburglar');
 const navbar = document.querySelector('.navbar');
 const heroArrow = document.querySelector('.hero__arrow');
 const subMenuMobile = document.querySelectorAll('.submenu__mobile');
+const main = document.querySelector('main');
+const video = document.querySelector('#specialty-video');
 
-
-
-document.addEventListener('DOMContentLoaded', (e) => {
+document.addEventListener('DOMContentLoaded', () => {
     new WOW().init();
-    [...faq].map(item => item.addEventListener('click', () => {
+    // faq //
+    faq.forEach(item => item.addEventListener('click', () => {
         item.classList.toggle('faq__list-item--active')
     }))
     
+    // header
     window.addEventListener('scroll', () => {
         let y = window.pageYOffset;
         y > 150 ? header.classList.add('scroll') : header.classList.remove('scroll')
         y > 300 ? heroArrow.classList.add('hero__arrow--hidden') : heroArrow.classList.remove('hero__arrow--hidden')
     })
     
+    // hero arrow
     heroArrow.addEventListener('click', () => {
         const headerHight = header.offsetHeight;
         const sectionOffset = document.querySelector('section').offsetHeight;
@@ -28,19 +31,33 @@ document.addEventListener('DOMContentLoaded', (e) => {
         })
     })
     
+    // burger
     burgerBtn.addEventListener('click', () => {
         if (burgerBtn.classList.contains('is-open')) {
             burgerBtn.classList.toggle('is-closed')
             navbar.classList.toggle('navbar--open')
+            main.classList.toggle('main--aside-active')
         } else {
+            main.classList.toggle('main--aside-active')
             burgerBtn.classList.toggle('is-open')
             navbar.classList.toggle('navbar--open')
+            main.addEventListener('click', () => {
+                if (burgerBtn.classList.contains('is-open')) {
+                    burgerBtn.classList.add('is-closed')
+                    navbar.classList.remove('navbar--open')
+                    main.classList.remove('main--aside-active')
+                }
+            })
         }
     });
     
-    [...subMenuMobile].map(item => {
+    // sub menu
+    subMenuMobile.forEach(item => {
         item.addEventListener('click', () => {
             item.classList.toggle('submenu__mobile--active')
         })
     })
+
+    // video
+    video.volume = .1;
 })
