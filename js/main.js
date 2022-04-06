592167039fe67745c72266cbbc22493be6a41b70
CREEZED <81812685+creezed@users.noreply.github.com>
2022-04-06 19:22:50 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,22 +4,45 @@ const burgerBtn = document.querySelector('.hamburglar');
 const navbar = document.querySelector('.navbar');
 const heroArrow = document.querySelector('.hero__arrow');
 const subMenuMobile = document.querySelectorAll('.submenu__mobile');
-const main = document.querySelector('main');
+const navbarBackground = document.querySelector('.navbar-background');
+const video = document.querySelector('#specialty-video');
+
+const hero = document.querySelector('.hero');
+
 
 
 document.addEventListener('DOMContentLoaded', () => {
     new WOW().init();
+
+    // bg
+    (function setHeroBg() {
+        let dateNow = new Date();
+        let getMonth = dateNow.getMonth() + 1;
+        
+        try
+        {
+            if (getMonth >= 3 && getMonth <= 8) {
+                hero.style.backgroundImage = "url('./assets/img/hero-bg-leto.webp')";
+            } else {
+                hero.style.backgroundImage = "url('./assets/img/hero-bg-zim.webp')";
+            }
+        } catch (e) {
+            return e;
+        }
+    })();
     // faq //
-    [...faq].map(item => item.addEventListener('click', () => {
+    faq.forEach(item => item.addEventListener('click', () => {
         item.classList.toggle('faq__list-item--active')
     }))
     
+    // header
     window.addEventListener('scroll', () => {
         let y = window.pageYOffset;
         y > 150 ? header.classList.add('scroll') : header.classList.remove('scroll')
         y > 300 ? heroArrow.classList.add('hero__arrow--hidden') : heroArrow.classList.remove('hero__arrow--hidden')
     })
     
+    // hero arrow
     heroArrow.addEventListener('click', () => {
         const headerHight = header.offsetHeight;
         const sectionOffset = document.querySelector('section').offsetHeight;
@@ -29,28 +52,40 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     })
     
+    // burger
     burgerBtn.addEventListener('click', () => {
         if (burgerBtn.classList.contains('is-open')) {
             burgerBtn.classList.toggle('is-closed')
             navbar.classList.toggle('navbar--open')
-            main.classList.toggle('main--aside-active')
+            navbarBackground.classList.toggle('navbar-background--active')
         } else {
-            main.classList.toggle('main--aside-active')
+            navbarBackground.classList.toggle('navbar-background--active')
             burgerBtn.classList.toggle('is-open')
             navbar.classList.toggle('navbar--open')
-            main.addEventListener('click', () => {
+            navbarBackground.addEventListener('click', () => {
                 if (burgerBtn.classList.contains('is-open')) {
                     burgerBtn.classList.add('is-closed')
                     navbar.classList.remove('navbar--open')
-                    main.classList.remove('main--aside-active')
+                    navbarBackground.classList.remove('navbar-background--active')
                 }
             })
         }
     });
     
-    [...subMenuMobile].map(item => {
+    // sub menu
+    subMenuMobile.forEach(item => {
         item.addEventListener('click', () => {
             item.classList.toggle('submenu__mobile--active')
         })
     })
+
+    // video
+    try {
+        video.volume = .2;
+    } catch (e) {
+        return e
+    }
+
+    // bg time
 })
+
